ce7c08bcb76cf3b0bea12dcde2fdcfd8493f000f
skytreader <chadestioco@gmail.com>
2012-10-07 14:44:13 +0800

    Solved rot13 (rotating Caesar Cypher Encryption with 13 places).

---
+++ cryptography/rot13.py
@@ -0,0 +1,31 @@
+#! usr/bin/env python
+
+"""
+http://programmingpraxis.com/2009/02/20/rot13/
+"""
+
+ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+
+def rot13(msg):
+	transformed = ""
+	lowered = msg.lower()
+	str_index = 0
+	
+	for letter in lowered:
+		if letter in ALPHABET:
+			letter_index = ALPHABET.index(letter)
+			transposed = (letter_index + 13) % len(ALPHABET)
+			
+			if letter != msg[str_index]:
+				transformed = "".join([transformed, ALPHABET[transposed].upper()])
+			else:
+				transformed = "".join([transformed, ALPHABET[transposed]])
+		else:
+			transformed = "".join([transformed, letter])
+	
+	return transformed
+
+if __name__ == "__main__":
+	print(rot13("The butler did it!"))
+	print(rot13("Gur ohgyre qvq vg!"))
+	print(rot13("Cebtenzzvat Cenkvf vf sha!"))
