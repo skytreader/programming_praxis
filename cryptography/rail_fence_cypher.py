eb4135bbaca3ce3b0b79de658a3f4ba759b3ec21
skytreader <chadestioco@gmail.com>
2012-10-08 23:10:22 +0800

    Rail fence now terminates (wow achievement?!).

--- cryptography/rail_fence_cypher.py
+++ cryptography/rail_fence_cypher.py
@@ -7,18 +7,35 @@ http://programmingpraxis.com/2009/03/31/rail-fence-cipher/
 def encrypt(message, key):
 	k = key
 	origin = 0
+	gap = 0
 	encrypted = ""
 	limit = len(message)
+	gap_limit = (key - 1) * 2
 	
 	while k != 0:
 		period = (k - 1) * 2
+		
+		if period == 0:
+			period = (key - 1) * 2
+		
 		index = origin
+		take_gap = False
 		
 		while index < limit:
 			encrypted += message[index]
-			index += period
+			
+			if take_gap and gap != 0:
+				index += gap
+			else:
+				index += period - origin
+			
+			take_gap = not take_gap
+			print("Inner loop " + str(index)) 
 		
 		origin += 1
+		k -= 1
+		gap += 1
+		print("Outer loop " + str(k))
 	
 	return encrypted
 
