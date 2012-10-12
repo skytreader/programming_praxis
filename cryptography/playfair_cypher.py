46f6d7ebb7b7d79ae00a0d655066e6c335f62527
skytreader <chadestioco@gmail.com>
2012-10-13 00:01:06 +0800

    Worked on digramming for playfair cypher.

--- cryptography/playfair_cypher.py
+++ cryptography/playfair_cypher.py
@@ -2,6 +2,11 @@
 
 PLAYFAIR_ALPHABET = "ABCDEFGHIKLMNOPQRSTUVWXYZ";
 
+"""
+Super assumption: all letters in the strings to be processed
+are in CAPITALS.
+"""
+
 def generate_key_square(key):
 	"""
 	Generates a 5-square for the given key. Returns
@@ -28,5 +33,41 @@ def generate_key_square(key):
 	
 	return key_square
 
+def digrammify(msg):
+	"""
+	Breaks msg into chunks of two characters each. If, in a chunk,
+	a letter repeats, we only take one letter and, for that chunk,
+	replace the next letter with an 'X'. The ommitted letter will
+	appear in the next chunk. We also append an 'X' if there is
+	less than two characters left for the last chunk.
+	
+	digrammify ignores spaces.
+	
+	@return A list of strings, each only two characters each.
+	"""
+	digram_chunks = []
+	# Strip spaces
+	msg = msg.replace(" ", "")
+	# Change all "J" to "I"
+	msg = msg.replace("J", "I")
+	
+	digram_size = 2
+	bound_limit = 2
+	max_limit = len(msg)
+	
+	while bound_limit <= max_limit:
+		# FIXME Ignore repeating letters in a chunk for now.
+		candidate = msg[bound_limit - digram_size:bound_limit]
+		
+		if candidate[0] == candidate[1]:
+			digram_chunks.append(candidate[0] + "X")
+			bound_limit += 1
+		else:
+			digram_chunks.append(msg[bound_limit - digram_size:bound_limit])
+			bound_limit += digram_size
+	
+	return digram_chunks
+
 if __name__ == "__main__":
 	print(generate_key_square("PLAYFAIR"))
+	print(digrammify("PROGRAMMING PRAXIS"))
