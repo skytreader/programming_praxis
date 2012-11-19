0a434938ed3c1f18d06884bfdbadeeb75597eee0
skytreader <chadestioco@gmail.com>
2012-11-19 18:14:20 +0800

    Changed approach to random text generation.

--- historical-computing/mark_v_shaney.py
+++ historical-computing/mark_v_shaney.py
@@ -1,14 +1,12 @@
 #! usr/bin/env python
 
 import unittest
+import random
 
 """
 http://programmingpraxis.com/2009/02/27/mark-v-shaney/
 UNFINISHED
 """
-
-# TODO Use a more efficient data structure to store the table.
-triple_table = []
 
 def save_triples(line):
 	"""
@@ -35,12 +33,39 @@ def train_on_text(text_filename):
 	For now, let consider each line separately. And consider
 	paragraphs and stuff.
 	"""
+	# TODO Use a more efficient data structure to store the table.
+	triple_table = []
+	
 	with open(text_filename) as training_text:
 		for line in text_filename:
 			triple_table.extend(save_triples(line))
-
-def generate_markov_probability():
 	
+	return triple_table
+
+def generate_random_text(text_triples, text_limit):
+	"""
+	Generate random text based on the given text_triples. The iteration
+	will run for the given text_limit.
+	"""
+	random_text = ""
+	last_word = ""
+	i = 0
+	
+	while i < text_limit:
+		if last_word == "":
+			triples_count = len(text_triples)
+			random_triple = text_triples[random.randint(0, triples_count)]
+			last_word = random_triple[1]
+			random_text = " ".join(random_triple)
+		else:
+			continuing_triple = list(filter(lambda triple: triple[1] == last_word, text_triples))
+			triples_count = len(continuing_triples)
+			random_triple = text_triples[random.randint(0, triples_count)]
+			random_text = " ".join(random_text.split(" ").extend(random_triple))
+		
+		i += 1
+	
+	return random_text
 
 class FunctionsTest(unittest.TestCase):
 	
