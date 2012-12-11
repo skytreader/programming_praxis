a02dcac0e4eaf84abf959f04f178cb3448d500d3
skytreader <chadestioco@gmail.com>
2012-12-11 18:55:55 +0800

    Added docstrings and finished encryption of double transposition cypher.

--- cryptography/double_transposition_cypher.py
+++ cryptography/double_transposition_cypher.py
@@ -8,6 +8,16 @@ UNFINISHED
 """
 
 def compute_numeric_key(word):
+	"""
+	Computes the numeric key of the given string. The characters are
+	sorted and then each character in the original word is replaced
+	by it's position in the sorted sequence. If a letter is repeated,
+	it's numeric replacement should be increasing left to right.
+
+	This uses Python's sorted function to sort.
+
+	Returns a numeric string.
+	"""
 	sorted_word = sorted(word)
 	numkey = []
 
@@ -19,6 +29,13 @@ def compute_numeric_key(word):
 	return "".join(numkey)
 
 def grid_laydown(plaintext, keylen):
+	"""
+	Lays down the given plaintext in a grid based on the given key
+	length. The text is laid-down left-to-right, top-to-bottom.
+
+	Returns a list of strings. Each string is one column. The columns are
+	arranged from left to right.
+	"""
 	grid = ["" for i in range(keylen)]
 	i = 0
 	limit = len(plaintext)
@@ -28,6 +45,17 @@ def grid_laydown(plaintext, keylen):
 		i += 1
 	
 	return grid
+
+def encrypt(plaintext, key):
+	numeric_key = compute_numeric_key(key)
+	grid_cols = len(key)
+	cipher_grid = grid_laydown(plaintext, grid_cols)
+	encrypted_blocks = [None for i in range(grid_cols)]
+
+	for i in range(grid_cols):
+		encrypted_blocks[i] = cipher_grid[int(numeric_key.index(str(i + 1)))]
+	
+	return "".join(encrypted_blocks)
 
 class FunctionsTest(unittest.TestCase):
 	"""
@@ -40,6 +68,9 @@ class FunctionsTest(unittest.TestCase):
 	def test_grid_laydown(self):
 		expected_output = ["pagi", "rmps", "omr", "gia", "rnx"]
 		self.assertEqual(expected_output, grid_laydown("programmingpraxis", 5))
+	
+	def test_encrypt(self):
+		self.assertEqual("omrpagigiarnxrmps", encrypt("programmingpraxis", "coach"))
 
 if __name__ == "__main__":
 	unittest.main()
