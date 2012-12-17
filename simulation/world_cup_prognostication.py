ab855cc5bbec3dba99a1e7943916e3190e878ff2
skytreader <chadestioco@gmail.com>
2012-12-17 18:18:25 +0800

    Revised approach to World Cup Simulation. Not yet runnable...problems with non-package importing!

--- simulation/world_cup_prognostication.py
+++ simulation/world_cup_prognostication.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 import math
 import random
@@ -8,6 +8,10 @@ from ../dry/dataset_reader import DataSetReader
 """
 http://programmingpraxis.com/2010/06/29/world-cup-prognostication/
 UNFINISHED
+
+Test cases:
+ - Even number of teams
+ - Odd number of teams
 """
 
 class Team(object):
@@ -54,24 +58,50 @@ def simulate_match(team_A, team_B):
 	
 	return better_team
 
-def package_binary_tree(teams):
-	"""
-	Given a list of teams where adjacent (even, odd) indices
-	are opponents for roud one, create a binary tree to
-	represent the whole tournament.
-
-	We assume zero-indexing.
-	"""
-	level_count = math.floor(math.log(len(teams), 2))
-
 def simulate_block(block):
 	"""
-	block is a list which we take to be a complete binary
-	tree (since the structure of a tournament is actually
-	a complete binary tree).
+	block is a list of teams. A team at an even-numbered index n
+	contests with the team at odd-numbered index n + 1. This
+	method returns the overall winner of the whole block.
 
-	Initially, only the bottom-most row of the binary tree
-	is filled. As teams win, they "float up" the tree until
-	one reaches the root ("the winner").
+	Note that a whole tournament may be represented as one
+	big block.
 	"""
-	pass
+	team_count = len(teams)
+	this_round = teams
+
+	while len(this_round) != 1:
+		skip_indices = range(len(this_round))
+		next_round = []
+		for i in skip_indices:
+			if i + 1 < team_count:
+				pass
+			else:
+				next_round.append(simulate_match(this_round[i], this_round[i + 1]))
+
+		this_round = next_round
+	
+	return this_round[0]
+
+if __name__ == "__main__":
+	# Create the competing teams.
+	brazil = Team("BRA", 2082)
+	spain = Team("ESP", 2061)
+	netherlands = Team("NED", 2045)
+	argentina = Team("ARG", 1966)
+	england = Team("ENG", 1945)
+	germany = Team("GER", 1930)
+	uruguay = Team("URU", 1890)
+	chile = Team("CHI", 1883)
+	portugal = Team("POR", 1874)
+	mexico = Team("MEX", 1873)
+	united_states = Team("USA", 1785)
+	paraguay = Team("PAR", 1771)
+	korea = Team("KOR", 1746)
+	japan = Team("JPN", 1744)
+	ghana = Team("GHA", 1711)
+	slovakia = Team("SVK", 1654)
+
+	tournament_block = [uruguay, korea, united_states, ghana, netherlands, slovakia, brazil, chile, argentina, mexico, germany, england, paraguay, japan, spain, portugal]
+	winner = simulate_block(tournament_block)
+	print("The winner is " + winner.country_code)
