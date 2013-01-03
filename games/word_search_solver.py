79996d53fb79355aafddea1afefd0a4ca0a73e14
skytreader <chadestioco@gmail.com>
2013-01-03 18:06:16 +0800

    Improved code for word search solver.

--- games/word_search_solver.py
+++ games/word_search_solver.py
@@ -5,6 +5,19 @@ http://programmingpraxis.com/2009/05/26/word-search-solver/
 UNFINISHED
 """
 
+# TAG
+# TODO Check if we can intergrate this pattern for PyGame Objects
+UP_LEFT = (-1, -1)
+UP = (-1, 0)
+UP_RIGHT = (-1, 1)
+LEFT = (0, -1)
+RIGHT = (0, 1)
+DOWN_LEFT = (1, -1)
+DOWN = (1, 0)
+DOWN_RIGHT = (1, 1)
+
+ALL_DIRECTIONS = [UP_LEFT, UP, UP_RIGHT, LEFT, RIGHT, DOWN_LEFT, DOWN, DOWN_RIGHT]
+
 def search_startswith(prefix, word_set):
     """
     Returns a list of all the words in word_set that starts
@@ -18,19 +31,40 @@ def search_startswith(prefix, word_set):
     
     return all_starts
 
-def get_neighbors(row, col):
+def step_in_directions(row, col, rowlimit, collimit, directions):
+    steps = []
+
+    for step_dir in directions:
+        drow = row + direction[0]
+        dcol = col + direction[1]
+
+        if (0 <= drow and drow < rowlimit) and (0 <= dcol and dcol < collimit):
+            steps.append((drow, dcol))
+
+    return steps
+
+def get_neighbors(row, col, rowlimit, collimit, direction = None):
     """
-    Returns a list of tuples, representing the neighbors of the given
-    cell, as described by row and col.
+    The return of this function varies from whether direction is set or
+    not. If direction is set, we return the neighbor adjacent to
+    (row, col) _in the specified direction_. If there are no more cells
+    accessible in that direction, None is returned.
+
+    If direction is not set, we simply return all adjacent neighbors to
+    the specified cell.
     """
-    if row == 0 and col == 0:
-        return [(0, 1), (1, 0), (1, 1)]
-    elif row == 0:
-        return [(row, col - 1), (row, col + 1), (row + 1, col - 1), (row + 1, col), (row + 1, col + 1)]
-    elif col == 0:
-        return [(row - 1, col), (row + 1, col), (row - 1, col + 1), (row, col + 1), (row + 1, col + 1)]
+    if direction:
+        return step_in_directions(row, col, rowlimit, collimit, [direction])
     else:
-        return [(row - 1, col - 1), (row - 1, col), (row - 1, col + 1), (row, col - 1), (row, col + 1), (row + 1, col - 1), (row + 1, col), (row + 1, col + 1)]
+        return step_in_directions(row, col, rowlimit, collimit, ALL_DIRECTIONS)
+
+def get_word(cell_seq, letter_block):
+    word = ""
+
+    for indices in cell_seq:
+        word += letter_block[indices[0]][indices[1]]
+
+    return word
 
 def search(word_set, letter_block):
     """
@@ -45,6 +79,15 @@ def search(word_set, letter_block):
      - intersecting starts: the initial letter of one word in word_set
        is found in the same cell as the initial letter of another word
        in word_set.
+
+    Let's try two approaches:
+      (1) Tag the neighbors of a prefix match cell on what direction are
+          they headed (up left, up, up right, ...,etc.).
+      (2) Use a tree instead of a stack in storing adjacent cells. The 
+          parent-child nodes of the tree will have a "comes from"
+          relationship. The tree must allow us access to a node's parent.
+          This way, we can derive from what direction do we proceed
+          (same as the direction a node's parent took).
     """
     block_height = len(letter_block)
     block_width = len(letter_block[0])
